e3bd8472d66bfb80a357d1802cd97550457a8b64
Jason-Twigg <twigg20@up.edu>
2021-03-26 19:45:41 -0700

    Bot now returns the image that you sent

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
 
 image_types = ["png", "jpeg", "gif", "jpg"]
+
+counter = 0
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -21,15 +24,26 @@ async def on_message(message: discord.Message):
     if message.content == "exit":
         await client.logout()
     
+    response = message.content
+    #counter = random.randint(0,10000)
+    found = False
+    filename = ""
     #response = "hi"
     #await message.channel.send(response)
     for attachment in message.attachments:
         if any(attachment.filename.lower().endswith(image) for image in image_types):
-            await attachment.save(attachment.filename)
+            filename = attachment.filename
+            await attachment.save(filename)
+            found = True
 
+    if not found: return
     #Save Attachment
     #Do work on images
     #Modify Image
+    if (response == ""): response = 'When you nut but she keeps sucking'
 
+    
+    
+    await message.channel.send(file=discord.File(filename))
 
 client.run(TOKEN)
