28648c1ae9daf0630cc87e4833a27f87d6ab7e72
gwonny <huangjo20@up.edu>
2021-03-27 19:06:30 -0700

    Added color customization, defaulting to white

--- img_utils.py
+++ img_utils.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy
+from textwrap import wrap
 from PIL import Image, ImageDraw, ImageFont
 
-def add_text(img, msg, fontsize, filename):
+def add_text(img, msg, fontsize, filename, color="white"):
     im = Image.open(img)
     W, H = im.size
     
@@ -21,12 +22,24 @@ def add_text(img, msg, fontsize, filename):
         style = ImageFont.truetype("impact.ttf", fontsize)
         w1, h1 = style.getsize(top)
         w2, h2 = style.getsize(bottom)
-        draw.text(((W-w1)/2,((H-h1)/2)/10), top, font=style, fill="white")
-        draw.text(((W-w2)/2, ((H-h2))*0.9), bottom, font=style, fill="white")
+        draw.text(((W-w1)/2,((H-h1)/2)*0.05), top, font=style, fill=color)
+        draw.text(((W-w2)/2, ((H-h2))*0.95), bottom, font=style, fill=color)
         im.save(filename)
     else:
         draw = ImageDraw.Draw(im)
         style = ImageFont.truetype("impact.ttf", fontsize)
         w, h = style.getsize(msg)
-        draw.text(((W-w)/2,((H-h)/2)/10), msg, font=style, fill="white")
-        im.save(filename)
+        if w > W*0.9:
+            max_width, _ = style.getsize("a")
+            max_size = (W*0.95)/max_width
+            print(max_size)
+            lines = wrap(msg, int(max_size))
+            y_text = ((H-h)/2)*0.05
+            for line in lines:
+                wrap_width, wrap_height = style.getsize(line)
+                draw.text(((W-wrap_width)/2, y_text), line, font=style, fill=color)
+                y_text += wrap_height
+            im.save(filename)
+        else:
+            draw.text(((W-w)/2,((H-h)/2)*0.05), msg, font=style, fill=color)
+            im.save(filename)
